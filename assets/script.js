7a8297d62dbce27ad333f95f79120f8f7677dc0c
Benjamin Brechter <benbrechter3@gmail.com>
2024-02-07 17:07:11 -0600

    I added everyhing

---
+++ assets/script.js
@@ -0,0 +1,22 @@
+var btnNo = document.getElementById('btn2')
+var btnYes = document.getElementById('btn1')
+var img1 = document.getElementById('img1')
+var ptag = document.getElementById('ptag')
+
+function four(){
+    btnYes.setAttribute("style", "font-size: 110px;")  
+}
+
+
+  
+btnNo.onclick = (event) =>{
+four()
+}
+
+btnYes.onclick = (event) => {
+img1.setAttribute("style", "display: none;")
+btnYes.setAttribute("style", "display: none;") 
+btnNo.setAttribute("style", "display: none;") 
+ptag.innerText = "It is A DATEEE!!!!!I am free after 9:30 I have to present my project :////. I def want to hang tho! "
+ptag.setAttribute('style', "font-size: 80px;")
+}
